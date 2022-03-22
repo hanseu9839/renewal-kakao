cc13878b3aad7d1514cf8da1a4c28e0bdb9778e5
hanseu9839 <hanseu9839@gmail.com>
2022-03-22 22:57:39 +0900

    socket message-error

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -11,7 +11,7 @@ export const getChat = async(req,res) => {
     });
     room.user.push(friend_id);
     room.save();
-    return res.render("chat",{pageTitle: "Chatting Room",room});
+    return res.status(200).render("chat",{pageTitle: "Chatting Room",room});
 };
 export const postChat = async(req,res)=>{
     const {user_id,friend_id} = req.params;
@@ -19,5 +19,5 @@ export const postChat = async(req,res)=>{
     if(!foundRoom){
         return res.render('/',{pageTitle:"Friend"});
     }
-    return res.render("chat",{pageTitle:"Chatting Room",foundRoom});
+    return res.status(200).render("chat",{pageTitle:"Chatting Room",foundRoom});
 }
--- src/server.js
+++ src/server.js
@@ -24,6 +24,39 @@ const server = http.createServer(app);
 const io = Socket(server);
 const PORT = 4040;
 
+io.on('connection',function(socket){
+  //socket에서 connection을 하면 DB의 room을 찾아준다. 그 후 room에서 stateMessage들을 다 집어넣어줌 
+  //populate를 써야 하는 거같음 왜냐하면 room에서는 message들의 값을 가져와야하기 떄문임
+  console.log('connect');
+    socket.on('disconnect',()=>{
+      console.log('user disconnected');
+    });
+    socket.on('joinRoom',async function(data){
+      socket.leave(socket.room);
+      socket.join(data.roomID);
+      socket.room = data.roomID;
+      console.log(data.roomID);
+      const findRoom = await Room.findById(data.roomID).populate("message");
+        for(let i=0;i<findRoom.message.length;i++){
+          const msg  = await Message.findById(findRoom.message[i]._id).populate("user");
+          io.to(socket.room).emit('preload',msg);
+        }
+    });
+    socket.on('message',async function(data){
+        const message = await Message.create({
+          text:data.message,
+          user:data.userID,
+          chatRoom:data.roomID,
+        });
+        const msg = await Message.findById(message._id).populate("user");
+        console.log(msg);
+        const room = await Room.findById(data.roomID);
+        room.message.push(message._id);
+        room.save();
+        io.to(socket.room).emit('message',msg);
+    });
+  });
+
 const handleLitening= () =>
     console.log(`Server connect on port http://loacalhost:${PORT}`); 
 app.set("view engine","pug");
@@ -55,37 +88,7 @@ app.use("/users",userRouter);
 app.use("/chat",chatRouter);
 app.use("/api",apiRouter);
 
-io.on('connection',function(socket){
-//socket에서 connection을 하면 DB의 room을 찾아준다. 그 후 room에서 stateMessage들을 다 집어넣어줌 
-//populate를 써야 하는 거같음 왜냐하면 room에서는 message들의 값을 가져와야하기 떄문임 
-  socket.on('disconnect',()=>{
-    console.log('user disconnected');
-  });
-  socket.on('joinRoom',async function(data){
-    socket.leave(socket.room);
-    socket.join(data.roomID);
-    socket.room = data.roomID;
-    console.log(data.roomID);
-    const findRoom = await Room.findById(data.roomID).populate("message");
-      for(let i=0;i<findRoom.message.length;i++){
-        const msg  = await Message.findById(findRoom.message[i]._id).populate("user");
-        io.to(socket.room).emit('preload',msg);
-      }
-  });
-  socket.on('message',async function(data){
-      const message = await Message.create({
-        text:data.message,
-        user:data.userID,
-        chatRoom:data.roomID,
-      });
-      const msg = await Message.findById(message._id).populate("user");
-      console.log(msg);
-      const room = await Room.findById(data.roomID);
-      room.message.push(message._id);
-      room.save();
-      io.to(socket.room).emit('message',msg);
-  });
-});
+
 server.listen(PORT,handleLitening);
 
 export default app;
