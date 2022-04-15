ad07c89e943ec2a8d63f35889806856c2cb4f25d
hanseu9839 <hanseu9839@gmail.com>
2022-04-15 22:45:49 +0900

    config variable

--- src/db.js
+++ src/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(process.env.DB_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+
 });
 
 const handleOpen= ()=>console.log("Connected to DB😎");
