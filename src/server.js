d75159f33300dd2187c339b3d75385519e0132a4
hanseu9839 <hanseu9839@gmail.com>
2022-04-18 01:27:53 +0900

    renewal-kakao

--- src/server.js
+++ src/server.js
@@ -64,11 +64,6 @@ const handleLitening= () =>
 app.set("view engine","pug");
 app.set("views",process.cwd() + "/src/views")
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.header("Cross-Origin-Opener-Policy", "same-origin");
-    next();
-  });
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
 
