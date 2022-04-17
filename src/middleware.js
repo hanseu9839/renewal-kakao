7ea007aa292e43a98d9305f8f6fe61a5cbcdfae1
hanseu9839 <hanseu9839@gmail.com>
2022-04-17 20:35:14 +0900

    aws-connection 01

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,6 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 export const localsMiddleware = (req,res,next) => {
    res.locals.loggedIn = Boolean(req.session.loggedIn);
    res.locals.siteName = "doongTalk";
